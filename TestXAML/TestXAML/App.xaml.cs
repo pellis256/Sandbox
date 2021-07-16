5eaaed4b73d82c9d4f8235ea03de1066ffab6b4f
pellis256 <peter.ellis@unchainedlabs.com>
2021-07-16 10:16:31 -0700

    Inspect XAML properties while debugging (sample)

---
+++ TestXAML/TestXAML/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestXAML
+{
+    ///Sample Link: https://docs.microsoft.com/en-us/visualstudio/xaml-tools/inspect-xaml-properties-while-debugging?view=vs-2019
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ TestXAML/TestXAML/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestXAML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        int count;
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem item = new ListBoxItem();
+            item.Content = "Item" + ++count;
+            if (count % 2 == 0)
+            {
+                item.Background = Brushes.LightGray;
+            }
+            else
+            {
+                item.Background = Brushes.LightYellow;
+            }
+            listBox.Items.Add(item);
+        }
+    }
+}
